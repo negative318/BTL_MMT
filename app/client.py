4facffcc3b955300a91cde1132eb65b24ad92d5f
qt04 <qthaipham2k4@gmail.com>
2024-11-03 22:47:42 +0700

    update_v4

--- app/client.py
+++ app/client.py
@@ -484,7 +484,7 @@ if __name__ == "__main__":
 
     try:
         ip = "192.168.1.10"
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect(("8.8.8.8", 80))
             ip = s.getsockname()[0]
         port = int(sys.argv[1])
